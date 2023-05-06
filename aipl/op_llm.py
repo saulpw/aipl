6fe8da1161dd6aca773903424a779a30f32aad21
Saul Pwanson <code@saul.pw>
2023-05-05 17:13:39 -0700

    [ops] !cluster

--- aipl/op_llm.py
+++ aipl/op_llm.py
@@ -35,10 +35,13 @@ def op_llm(aipl, v:str, **kwargs) -> str:
     return resp['choices'][0]['message']['content']
 
 
-@defop('llm-embedded', 0, 0, 1)
+@defop('llm-embedding', 0, 0, 1)
 @expensive
 def op_llm_embedding(aipl, v:str, **kwargs) -> List[float]:
     import openai
+
+    if not v:
+        raise Exception('no content for embedding')
 
     resp = openai.Embedding.create(input=v, **kwargs)
 
@@ -46,3 +49,16 @@ def op_llm_embedding(aipl, v:str, **kwargs) -> List[float]:
     stderr(f'Used {used} tokens')
 
     return resp['data'][0]['embedding']
+
+
+@defop('cluster', 1, 1, 1)
+def op_cluster(aipl, v:List[List[float]], n=10):
+    'Find n clusters in the input vectors'
+    import numpy as np
+    from sklearn.cluster import KMeans
+
+    matrix = np.vstack(v)
+    kmeans = KMeans(n_clusters=n, init='k-means++', random_state=42, n_init='auto')
+    kmeans.fit(matrix)
+
+    return kmeans.labels_
