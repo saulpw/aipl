e6e685ffbb534d43e6c2cc789b13ecf6dc00904b
Saul Pwanson <code@saul.pw>
2023-05-07 14:27:28 -0700

    add __init__ with all operators

---
+++ aipl/__init__.py
@@ -0,0 +1,7 @@
+from .interpreter import AIPLInterpreter
+from .op_text import *
+from .op_web import *
+from .op_llm import *
+from .op_misc import *
+from .op_db import *
+from .op_func import *
--- aipl/__main__.py
+++ aipl/__main__.py
@@ -1,13 +1,10 @@
+from typing import List
+
 import os
 import sys
 
 from .interpreter import AIPLInterpreter
-from .op_text import *
-from .op_web import *
-from .op_llm import *
-from .op_misc import *
-from .op_db import *
-from .op_func import *
+from .table import LazyRow
 
 
 def duptty():
