f7389dc22a3e75a90ace44f64e3a3cf986befeaa
Saul Pwanson <code@saul.pw>
2023-05-25 23:09:01 -0700

    [ops] move !!def into def.py

--- aipl/ops/def.py
+++ aipl/ops/def.py
@@ -1,13 +1,17 @@
-from copy import copy
-from typing import List, Dict
-from collections import defaultdict
+'''
+!!def <opname>
+ !op1
+ !op2
 
-from aipl import defop
-from aipl.table import Table, LazyRow, Column, ParentColumn
+Create a new op named <opname> that runs the AIPL in the prompt when invoked.
+'''
+
+from aipl import defop, Table
 
 
 @defop('def', None, None, arity=0)  # immediate
 def op_def(aipl, opname, prompt=''):
+    'Define composite operator from cmds in prompt (must be indented).'
     cmds = aipl.parse(prompt)
 
     @defop(opname,
@@ -18,3 +22,15 @@ def op_def(aipl, opname, prompt=''):
         argkey = aipl.unique_key
         ret = aipl.run_cmdlist(cmds, Table([{argkey:input}]), *args)
         return ret[0]
+
+
+def test_def(aipl):
+    r = aipl.run('''
+!!def split-join
+ !split
+ !join
+
+!split-join
+''', 'a b c', 'd e f')
+    assert r[0] == 'a b c'
+    assert r[1] == 'd e f'
