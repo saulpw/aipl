79fa2ec69087d695daf256bfc24fbe0adf39c126
anjakefala <anja.kefala@gmail.com>
2023-05-28 21:42:28 -0700

    [ops] move cluster, columns, filter, and llm into their own files; add docstrings

--- aipl/ops/llm.py
+++ aipl/ops/llm.py
@@ -1,3 +1,10 @@
+'''
+!llm and !llm-embedding use the OpenAI API
+to make queries to their llm models.
+
+Require the setting of an OPENAI_API_KEY.
+'''
+
 from typing import List, Dict
 
 from aipl import defop, expensive, stderr
@@ -15,6 +22,7 @@ def _parse_msg(s:str):
 @defop('llm', 0, 0, 1)
 @expensive
 def op_llm(aipl, v:str, **kwargs) -> str:
+    'Send a chat message to an OpenAI LLM. Supports [all params](https://platform.openai.com/docs/guides/chat/introduction).'
     import openai
     parms = dict(
         temperature=0,
@@ -37,6 +45,7 @@ def op_llm(aipl, v:str, **kwargs) -> str:
 @defop('llm-embedding', 0, 0.5, 1)
 @expensive
 def op_llm_embedding(aipl, v:str, **kwargs) -> dict:
+    'Get a [text embedding](https://platform.openai.com/docs/guides/embeddings/what-are-embeddings) for a string: a measure of text-relatedness, to be used with e.g. !cluster.'
     import openai
 
     if not v:
@@ -50,16 +59,3 @@ def op_llm_embedding(aipl, v:str, **kwargs) -> dict:
     return dict(model=kwargs.get('model'),
                 used_tokens=used,
                 embedding=resp['data'][0]['embedding'])
-
-
-@defop('cluster', 1, 1, 1)
-def op_cluster(aipl, v:List[List[float]], n=10):
-    'Find n clusters in the input vectors'
-    import numpy as np
-    from sklearn.cluster import KMeans
-
-    matrix = np.vstack(v)
-    kmeans = KMeans(n_clusters=n, init='k-means++', random_state=42, n_init='auto')
-    kmeans.fit(matrix)
-
-    return [int(x) for x in kmeans.labels_]
