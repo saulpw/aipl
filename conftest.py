79a661c5727f191ba9aa8d689851344cce445b70
anjakefala <anja@voltrondata.com>
2023-05-31 17:18:39 -0700

    [tests] have default return for AttrDict instead of parsing args for pytest

--- conftest.py
+++ conftest.py
@@ -1,10 +1,8 @@
 import pytest
 
-from aipl.main import parse_args
 from aipl import AIPL
 
 @pytest.fixture()
 def aipl():
-    args = parse_args(['-d', '--script-or-global foo=bar'])
-    r = AIPL(**vars(args))
+    r = AIPL(debug=True)
     return r
