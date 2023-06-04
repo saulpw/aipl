76db8a0e19b50a943bb8b24e87d4de6576502976
Adam Kelly <adam@cthulahoops.org>
2023-06-04 17:37:24 +0100

    Lark parser that can generate an AST for all current tests/chains.

---
+++ aipl/parser.py
@@ -0,0 +1,87 @@
+from typing import List
+import textwrap
+import sys
+from dataclasses import dataclass
+
+from lark import Lark, Transformer
+
+aipl_grammar = Lark(r'''
+%ignore " "
+
+start: line*
+
+line: command | "\n"
+command: (COMMAND | IMMEDIATE_COMMAND) IDENTIFIER redirection arg* "\n" prompt
+
+COMMAND: "!"
+IMMEDIATE_COMMAND: "!!"
+
+redirection: ( ">" IDENTIFIER? )*
+
+arg: KEY "=" VALUE | VALUE
+
+VALUE: /\S+/
+KEY: IDENTIFIER
+
+IDENTIFIER: /[A-Za-z0-9_-]+/
+
+prompt: STRING_LINE*
+STRING_LINE: [ /[^!#][^\n]*/ ] "\n"
+
+COMMENT_LINE: /^#[^\n]*\n/m
+
+%ignore COMMENT_LINE
+''')
+
+
+@dataclass
+class AstCommand:
+    opname:str
+    varnames:List[str]
+    immediate:bool
+    args:list
+    kwargs:dict
+    line:str
+    linenum:int
+
+class ToAst(Transformer):
+    def line(self, tree):
+        return tree
+
+    def start(self, tree):
+        return [command for command in tree if command]
+
+    def command(self, tree):
+        print("TREE:", tree)
+        command_sign, opname, varnames, *args = tree
+
+        return AstCommand(
+            opname=tree[1].value,
+            line=None, # TODO Not yet preserving line contents.
+            linenum=command_sign.line,
+            immediate=command_sign.type == 'IMMEDIATE_COMMAND',
+            varnames=varnames,
+            args=[v for k, v in args if k is None and v is not None],
+            kwargs={k: v for k, v in args if k is not None})
+
+    def redirection(self, tree):
+        return [token.value for token in tree]
+
+    def arg(self, tree):
+        if tree[0].type == 'KEY':
+            return (tree[0].value, tree[1].value)
+        return (None, tree[0].value)
+
+    def prompt(self, lines):
+        prompt = textwrap.dedent('\n'.join(token.value for token in lines).strip())
+        if not prompt:
+            return (None, None)
+        return ('prompt', prompt)
+
+
+if __name__ == '__main__':
+    for file in sys.argv[1:]:
+        print("Parsing: ", file)
+        parse_tree = aipl_grammar.parse(open(file).read())
+        for command in ToAst().transform(parse_tree):
+            print(command)
