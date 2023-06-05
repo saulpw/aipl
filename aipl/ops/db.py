2162e22023f10e68c2346759edd48a7d7a5b6c55
Saul Pwanson <code@saul.pw>
2023-06-04 22:48:40 -0700

    [ops] add !!dbopen and !!dbquery; make !db* ops use output_db

--- aipl/ops/db.py
+++ aipl/ops/db.py
@@ -1,13 +1,25 @@
-from aipl import defop
+from aipl import defop, Database
 
 
-@defop('dbinsert', 0.5, None, 1)
-def op_dbinsert(aipl, row, tblname, **kwargs):
-    'Insert each row into database table.'
-    aipl.output_db.insert(tblname, **row._asdict(), **kwargs)
+@defop('dbopen', None, 0)
+def op_dbopen(aipl, url:str):
+    ''
+    return Database(url)
+
+@defop('dbquery', 0.5, 1.5)
+def op_dbquery(aipl, row:'LazyRow', dbname:str, tblname:str, *colnames, **kwargs):
+    ''
+    for r in aipl.globals[dbname].select(tblname, **kwargs):
+        yield {colname:r[colname] for colname in colnames}
 
 
 @defop('dbdrop', None, None, 0)
-def op_dbdrop(aipl, tblname):
+def op_dbdrop(aipl, tblname:str):
     'Drop database table.'
     aipl.output_db.sql(f'DROP TABLE IF EXISTS {tblname}')
+
+
+@defop('dbinsert', 0.5, None)
+def op_dbinsert(aipl, row, tblname:str, **kwargs):
+    'Insert each row into database table.'
+    aipl.output_db.insert(tblname, **row._asdict(), **kwargs)
