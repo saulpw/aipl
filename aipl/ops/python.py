6687efb3299157b6f303f13998f8426637e3086c
Saul Pwanson <code@saul.pw>
2023-06-04 22:49:18 -0700

    [ops] add !python-expr to eval over each row

--- aipl/ops/python.py
+++ aipl/ops/python.py
@@ -4,8 +4,13 @@ from aipl import defop
 @defop('python',None,None,0)
 def op_python(aipl, prompt:str=''):
     'exec() Python toplevel statements.'
-    exec(prompt)
+    aipl.globals['defop'] = defop
+    exec(prompt, aipl.globals)
 
+@defop('python-expr', 0.5, 0)
+def op_python_expr(aipl, row, expr:str):
+    'Add columns for Python expressions.'
+    return eval(expr, aipl.globals, row)
 
 @defop('python-input', 100, 1.5, outcols='pyval')
 def op_python_input(aipl, t:'Table', prompt:str=''):
