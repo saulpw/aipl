c379d254457192d191a439eda624d3197236f556
anjakefala <anja.kefala@gmail.com>
2023-06-05 20:36:12 -0700

    [require-input-] check value column; needed since e629f48c02f38

--- aipl/ops/input.py
+++ aipl/ops/input.py
@@ -10,7 +10,7 @@ from aipl import defop, Table
 @defop('require-input', 100, 100)
 def op_require_input(aipl, t:'Table', prompt=''):
     'Ensure there is any input at all; if not, display the prompt and read input from the user.'
-    if len(t.rows) == 0 or not t[0]:
+    if len(t.rows) == 0 or not t[0].value:
         print(prompt, file=sys.stderr)
         print('Ctrl+D to end input', file=sys.stderr)
         return Table([{'input':sys.stdin.read().strip('\n')}])
