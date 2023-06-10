67addaa56e6a1cda6d23e163fe09db91c70dbdd2
anjakefala <anja.kefala@gmail.com>
2023-06-09 22:00:05 -0700

    [docs] add docstrings for abort, db, literal, option, replace

--- aipl/ops/abort.py
+++ aipl/ops/abort.py
@@ -1,6 +1,7 @@
-from aipl import defop
+from aipl import defop, UserAbort
 
 
 @defop('abort', None, None, arity=0)
 def op_abort(aipl, *args):
+    "Abort the current chain."
     raise UserAbort(*args)
