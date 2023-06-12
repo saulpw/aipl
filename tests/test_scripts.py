c1c56f11caf5d6dde24912c706664ebd34a37399
anjakefala <anja.kefala@gmail.com>
2023-06-11 21:54:55 -0700

    [tests] turn test-input,-equal,-json on for testrunner

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -6,6 +6,7 @@ from aipl.interpreter import AIPL
 @pytest.mark.parametrize("input_file", glob("tests/*.aipl"))
 def test_script(aipl, input_file):
     aipl.stdout = StringIO()
+    aipl.options.test = True
 
     with open(input_file) as fh:
         aipl.run_test(fh.read(), '')
