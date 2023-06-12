3bebe0d822fba159c63278677379819b20050099
Rowan Copley <rowan.copley@gmail.com>
2023-06-12 12:28:09 -0700

    fix typing

--- aipl/ops/csv.py
+++ aipl/ops/csv.py
@@ -1,8 +1,9 @@
-from aipl import defop, Table, Column
+from aipl import defop
+from typing import List
 
 # assumes header row
 @defop('csv-parse', None, 1.5)
-def op_csv_load(aipl, fname:str) -> dict:
+def op_csv_load(aipl, fname:str) -> List[dict]:
     import csv
     with open(fname, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
