54ccbf5155658786106746175d0e68b352ccb75c
Rowan Copley <rowan.copley@gmail.com>
2023-06-12 12:26:57 -0700

    switch to yielding rows

--- aipl/ops/csv.py
+++ aipl/ops/csv.py
@@ -4,9 +4,7 @@ from aipl import defop, Table, Column
 @defop('csv-parse', None, 1.5)
 def op_csv_load(aipl, fname:str) -> dict:
     import csv
-    ret = []
     with open(fname, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            ret.append(row)
-    return ret
+            yield row
