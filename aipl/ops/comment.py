384474e45061e2c7ad877114b7c23e305a2c4051
Saul Pwanson <code@saul.pw>
2023-07-17 23:44:01 -0700

    [docs] fixes

--- aipl/ops/comment.py
+++ aipl/ops/comment.py
@@ -3,5 +3,5 @@ from aipl import defop
 
 @defop('comment', None, None)
 def op_comment(aipl, *args, **kwargs):
-    'Do nothing (ignoring args and prompt).'
+    'Do nothing (ignore args and prompt).'
     pass
